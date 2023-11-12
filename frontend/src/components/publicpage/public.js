2a02ce5607ee8dde348b8d343da95f009b20c57c
chenvincent7629 <chenvincent7629@gmail.com>
2023-11-12 02:35:10 -0800

    more frontend

---
+++ frontend/src/components/publicpage/public.js
@@ -0,0 +1,85 @@
+import { Link } from 'react-router-dom'
+import React from 'react';
+import Cardsearch from '../buttons/searchbar';
+import Banlist from '../buttons/banlist';
+import Mycards from '../buttons/my-cards';
+import Aboutus from '../buttons/about-us';
+import Privacypolicy from '../buttons/Privacypolicy';
+import ContactInfo from '../buttons/contactinfo';
+import Home from '../buttons/home';
+import Login from '../buttons/login';
+
+
+
+
+const Public = () => {
+   
+    const content = (
+        <section className="public">
+            <header className="header">
+                <ul className="Menu-container-left">
+                    <li>
+                        <Cardsearch/> {/*button component to redirect to /search endpoint */}
+                    </li>
+                    <li>
+                        <Banlist/> {/*button component to redirect to /banlist endpoint*/}
+                    </li>
+                </ul>
+                <div className="h1-container">
+                    <h1>DeckDatabaseOnline</h1>
+                </div>
+                <ul className='Menu-container-right'>
+                    <li>
+                        <Mycards/> {/*button component to redirect to /my-cards endpoint*/}
+                    </li>
+                    <li>
+                        <Login/> {/*button component to redirect to /about-us endpoint*/}
+                    </li>
+                </ul>
+            </header>
+            <banner>
+                <div className="banner"></div>
+            </banner> 
+            <body>
+                <h1 className= "h1titlebox">
+                    Welcome to the My Deck Database website
+                </h1>
+                
+                <div className='body-container'>
+                    <div className="homepage-parallelogram-upperleft"></div>
+                    <div className="homepage-parallelogram-bottomleft"></div>
+                    <div className="homepage-parallelogram-upperright"></div>
+                    <div className="homepage-parallelogram-bottomright"></div>
+                    <div className="tech-circle"></div>
+                </div>
+            </body>
+            <footer>
+                <div  className="footer-container">
+                    <p classname="copyright-container">&copy; 2023 DeckDatabaseOnline. All rights reserved.</p>   
+                    <ul className="footer-links">
+                        <li>
+                            <Privacypolicy/> {/*button component to redirect to /privacy-policy endpoint */}
+                        </li>
+                        <li>
+                            <ContactInfo/> {/*button component to redirect to /contact-info endpoint */}
+                        </li> 
+                        <li>
+                            <Aboutus/> {/*button component to redirect to /login endpoint */}
+                        </li>
+                        <li>
+                            <Home/> {/*button component to redirect to / endpoint */}
+                        </li>
+                        <li>
+                            Click for Surprise
+                        </li>
+                    </ul>
+                    
+                </div>
+            </footer>
+        </section>
+    )
+    return content
+}
+
+export default Public
+
--- frontend/src/features/auth/login.js
+++ frontend/src/features/auth/login.js
@@ -1,6 +1,14 @@
+import DashFooter from "../../components/dash/dashfooter"
+
+
 const Login = () => {
     return (
+    <>
+
         <h1>Login</h1>
+
+        <DashFooter/>
+    </>
     )
 }
 
