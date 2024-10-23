73a5f0a8220099652f9ef2167d0bede469fceb3f
chenvincen <71801988+chenvincen@users.noreply.github.com>
2024-10-23 01:23:47 -0700

    Accepted incomming changes

--- frontend/src/features/pages/homepage/homepage.js
+++ frontend/src/features/pages/homepage/homepage.js
@@ -1,10 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import "./styling/body.css"
-import "./styling/banner.css"
-import "./styling/parallelogram.css"
-=======
->>>>>>> 14ef751 (testing)
 import Header from '../../../components/header/header';
 import Footer from '../../../components/footer/Footer';
 
@@ -15,15 +9,6 @@ const HomePage = () => {
     const content = (
         <section className="public">
             <Header/>
-<<<<<<< HEAD
-            <main className="Homepage-body-container">
-                <div className= "homepagetitlebox">Welcome to DeckDataBaseOnline!</div>
-                <div className="homepagedescriptionbox"> 
-                    All-in one website allowing you to catalog your card-collection and create 
-                    custom decks
-                </div>
-                <div className="tech-circle"></div>
-=======
             <main className="top-[400px] w-100 h-[83vh] bg-radial-gray">
                 <div 
                     className= "absolute left-[15%] top-[30%] w-[30%] font-black text-left text-8xl text-gold"
@@ -37,7 +22,6 @@ const HomePage = () => {
                     custom decks
                 </div>
                 <div className="bg-[url('../img/dragonicon.png')] absolute top-1/4 left-[45%] bg-center bg-contain bg-no-repeat h-[45%] w-[45%]"/>
->>>>>>> 14ef751 (testing)
             </ main>
             <Footer/>
         </section>
