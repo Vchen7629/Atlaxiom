f282ad10668dbdfaced11e76a90ca501db706cf0
chenvincen <71801988+chenvincen@users.noreply.github.com>
2025-01-20 21:53:04 -0800

    Changed port to 443 to listen for https requests on backend server

--- login api/config/allowedOrigins.js
+++ login api/config/allowedOrigins.js
@@ -4,6 +4,8 @@ const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://localhost:3005",
+    "https://localhost:3005",
+    "https://localhost:443",
     "http://localhost:8080",
     "http://atlaxiom.com",
     "http://www.atlaxiom.com"
