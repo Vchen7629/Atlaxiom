fdc8801407a3be6e564e9d68e90abe3f3138ccd8
chenvincen <71801988+chenvincen@users.noreply.github.com>
2025-01-21 20:19:14 -0800

    Testing to see if adding headers to each route works

--- loginapi/server.js
+++ loginapi/server.js
@@ -23,7 +23,7 @@ connectDB().catch(err => {
     process.exit(1); // Exit the process if DB connection fails
 });
 
-app.use('/auth', (res, next) => {
+app.options('/auth', (res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://api.atlaxiom.com');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS'); 
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); 
