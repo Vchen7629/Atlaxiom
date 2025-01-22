a17605cffe1378912ba682b08a016dafa90ebb44
chenvincen <71801988+chenvincen@users.noreply.github.com>
2025-01-21 19:34:55 -0800

    Cors

--- loginapi/config/corsOptions.js
+++ loginapi/config/corsOptions.js
@@ -10,7 +10,7 @@ const corsOptions = {
     },
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Range', 'Authorization'],
     optionsSuccessStatus: 200,
 }
 
