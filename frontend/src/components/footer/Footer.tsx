94bbe34d222a12ca08a0e03a0e8332b4840c2dba
chenvincen <71801988+chenvincen@users.noreply.github.com>
2025-01-23 12:57:18 -0800

    Modified header to be responsive

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -10,9 +10,9 @@ const Footer = () => {
             <div className="bg-blackone flex flex-col w-full h-[20vh] items-center justify-between pb-2 text-lg pt-8"> 
                 <div className="flex w-[90%] h-1/2 mb-2 items-center justify-between">
                     <div className="flex justify-between">
-                        <div className="font-bold text-goldenrod text-[40px]">Atlaxiom</div>
+                        <div className="font-bold text-goldenrod sm:text-sm md:text-[40px]">Atlaxiom</div>
                     </div>
-                    <div className="flex w-[30%] justify-between">
+                    <div className="relative w-[40%] hidden sm:block md:flex justify-between">
                         <AboutUs/>
                         <PrivacyPolicy/>
                         <Home/>
@@ -23,12 +23,12 @@ const Footer = () => {
                     </div>
                 </div> 
 
-                <div className="text-sm text-gray-400 w-3/4">
+                <div className="text-sm  text-gray-400 w-3/4">
                     The literal and graphical information presented on this site about Yu-Gi-Oh!, 
-                including card images, the attribute, level/rank and type symbols, and card text, 
-                is copyright 4K Media Inc, a subsidiary of Konami Digital Entertainment, Inc. This 
-                website is not produced by, endorsed by, supported by, or affiliated with 4k Media or 
-                Konami Digital Entertainment.
+                    including card images, the attribute, level/rank and type symbols, and card text, 
+                    is copyright 4K Media Inc, a subsidiary of Konami Digital Entertainment, Inc. This 
+                    website is not produced by, endorsed by, supported by, or affiliated with 4k Media or 
+                    Konami Digital Entertainment.
                 </div>
             </div>
            
