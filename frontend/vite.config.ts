3c12ec41fb002f4b37be707f1ea871777166bb0e
chenvincen <71801988+chenvincen@users.noreply.github.com>
2025-01-26 11:44:16 -0800

    optimizing vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-//import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +10,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    include: ["react", "react-dom"], 
+  },
   build: {
+    minify: "esbuild",
     rollupOptions: {
       output: {
         manualChunks: {
