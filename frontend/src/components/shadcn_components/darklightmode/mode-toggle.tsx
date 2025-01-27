720593366e17302a86320d3d50e165bede2e5204
chenvincen <71801988+chenvincen@users.noreply.github.com>
2025-01-26 23:02:56 -0800

    Changed header and homepage styling/ui

--- frontend/src/components/shadcn_components/darklightmode/mode-toggle.tsx
+++ frontend/src/components/shadcn_components/darklightmode/mode-toggle.tsx
@@ -15,7 +15,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild>
-        <Button variant="cardcollection" className="h-9 w-11 md:h-10 md:w-12 rounded-xl bg-[hsl(var(--background3))]">
+        <Button variant="cardcollection" className="border-2 border-[hsl(var(--border))] h-9 w-11 md:h-10 md:w-12 rounded-xl bg-[hsl(var(--background3))]">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-gold" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
