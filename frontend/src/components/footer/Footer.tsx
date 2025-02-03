3352e116e81766b79437cde1c3f2317c6d18d631
chenvincen <71801988+chenvincen@users.noreply.github.com>
2025-02-03 00:05:00 -0800

    made footer mobile responsive

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -10,35 +10,58 @@ import Signup from "./QuickLinkButtons/Signup.tsx"
 const Footer = () => {
     
     const content = (
-        <footer className="z-50 bg-blackone flex w-full space-x-[5vw] min-h-[25vh] pb-2 text-lg pt-[5vh] px-[10vw]">
-            <div className="flex flex-col w-[25vw] h-fit space-y-[1.5vh]">
+        <footer className="z-50 bg-blackone flex flex-col lg:flex-row w-full space-x-[5vw] min-h-[25vh] pb-2 text-lg pt-[5vh] px-[10vw]">
+            <div className="flex flex-col w-full text-center lg:text-start lg:w-[25vw] h-fit space-y-[1.5vh]">
                 <span className="font-bold text-goldenrod text-4xl">Atlaxiom</span>
                 <span className="text-sm">Atlaxiom is a all-in-one Yu-Gi-Oh! Database, Deck Builder and Collection Manager web app!</span>
-                <div className="flex w-full space-x-2 items-center">
+                <div className="flex w-full space-x-2 items-center justify-center lg:justify-normal">
                     <span className="text-white text-md">To Get Started</span>
                     <Login/>
                     <span>or</span>
                     <Signup/>
                 </div>
             </div>
-            <div className="flex flex-col w-[15vw] h-fit space-y-[1.5vh]">
+            <div className="hidden lg:flex flex-col w-[15vw] h-fit space-y-[1.5vh]">
                 <span className="text-xl font-bold text-white">Quick Links</span>
                 <Home/>
                 <CardSearch/>
             </div>
-            <div className="flex flex-col w-[15vw] h-fit space-y-[1.5vh]">
+            <div className="hidden lg:flex flex-col w-[15vw] h-fit space-y-[1.5vh]">
                 <span className="text-xl font-bold text-white">Support</span>
                 <FAQ/>
                 <ContactUs />
             </div>
-            <div className="flex flex-col w-[15vw] h-fit space-y-[1.5vh]">
+            <div className="hidden lg:flex flex-col w-[15vw] h-fit space-y-[1.5vh]">
                 <span className="text-xl font-bold text-white">Social</span>
                 <Github/>
                 <ContactUs />
             </div>
-            <div className="flex flex-col w-[15vw] h-fit space-y-[1.5vh]">
+            <div className="hidden lg:flex flex-col w-[15vw] h-fit space-y-[1.5vh]">
                 <span className="text-xl font-bold text-white">Legal</span>
                 <PrivacyPolicy/>
+            </div>
+            <div className="flex lg:hidden w-full mt-[5vh] space-x-[5vw]">
+                <div className="flex flex-col w-[35vw] h-fit space-y-[1.5vh]">
+                    <span className="text-xl font-bold text-white">Quick Links</span>
+                    <Home/>
+                    <CardSearch/>
+                </div>
+                <div className="flex  flex-col w-[35vw] h-fit space-y-[1.5vh]">
+                    <span className="text-xl font-bold text-white">Support</span>
+                    <FAQ/>
+                    <ContactUs />
+                </div>  
+            </div>
+            <div className="flex lg:hidden w-full mt-[5vh] space-x-[5vw]">
+                <div className="flex flex-col w-[35vw] h-fit space-y-[1.5vh]">
+                    <span className="text-xl font-bold text-white">Social</span>
+                    <Github/>
+                    <ContactUs />
+                </div>
+                <div className="flex flex-col w-[35vw] h-fit space-y-[1.5vh]">
+                    <span className="text-xl font-bold text-white">Legal</span>
+                    <PrivacyPolicy/>
+                </div>
             </div>
         </footer>
     )
